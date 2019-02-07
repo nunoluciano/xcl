fcf57c99f8e41c5c035fb5ab702cb0739aaa35d0
Info Opensource <info.opensource@gmail.com>
2019-02-07 12:25:34 +0100

    Update version /include

--- html/include/xoopscodes.php
+++ html/include/xoopscodes.php
@@ -37,12 +37,17 @@ function xoopsCodeTarea($textarea_id, $cols=60, $rows=15, $suffix=null)
 {
     $hiddentext = isset($suffix) ? 'xoopsHiddenText'.trim($suffix) : 'xoopsHiddenText';
     //Hack for url, email ...., the anchor is for having a link on [_More...]
-    echo "<a name='moresmiley'></a><img src='".XOOPS_URL."/images/url.gif' alt='url' onmouseover='style.cursor=\"hand\"' onclick='xoopsCodeUrl(\"$textarea_id\", \"".htmlspecialchars(_ENTERURL, ENT_QUOTES)."\", \"".htmlspecialchars(_ENTERWEBTITLE, ENT_QUOTES)."\");'/>&nbsp;<img src='".XOOPS_URL."/images/email.gif' alt='email' onmouseover='style.cursor=\"hand\"' onclick='xoopsCodeEmail(\"$textarea_id\", \"".htmlspecialchars(_ENTEREMAIL, ENT_QUOTES)."\");' />&nbsp;<img src='".XOOPS_URL."/images/imgsrc.gif' alt='imgsrc' onmouseover='style.cursor=\"hand\"' onclick='xoopsCodeImg(\"$textarea_id\", \"".htmlspecialchars(_ENTERIMGURL, ENT_QUOTES)."\", \"".htmlspecialchars(_ENTERIMGPOS, ENT_QUOTES)."\", \"".htmlspecialchars(_IMGPOSRORL, ENT_QUOTES)."\", \"".htmlspecialchars(_ERRORIMGPOS, ENT_QUOTES)."\");' />&nbsp;<img src='".XOOPS_URL."/images/image.gif' alt='image' onmouseover='style.cursor=\"hand\"' onclick='openWithSelfMain(\"".XOOPS_URL."/imagemanager.php?target=".$textarea_id."\",\"imgmanager\",400,430);' />&nbsp;<img src='".XOOPS_URL."/images/code.gif' alt='code' onmouseover='style.cursor=\"hand\"' onclick='xoopsCodeCode(\"$textarea_id\", \"".htmlspecialchars(_ENTERCODE, ENT_QUOTES)."\");' />&nbsp;<img src='".XOOPS_URL."/images/quote.gif' alt='quote' onmouseover='style.cursor=\"hand\"' onclick='xoopsCodeQuote(\"$textarea_id\");'/><br />\n";
+    echo "<a name='moresmiley'></a><img src='".XOOPS_URL."/images/url.gif' alt='url' onmouseover='style.cursor=\"hand\"' onclick='xoopsCodeUrl(\"$textarea_id\", \"".htmlspecialchars(_ENTERURL, ENT_QUOTES)."\", \"".htmlspecialchars(_ENTERWEBTITLE, ENT_QUOTES)."\");'/>
+    &nbsp;<img src='".XOOPS_URL."/images/email.gif' alt='email' onmouseover='style.cursor=\"hand\"' onclick='xoopsCodeEmail(\"$textarea_id\", \"".htmlspecialchars(_ENTEREMAIL, ENT_QUOTES)."\");' />
+    &nbsp;<img src='".XOOPS_URL."/images/imgsrc.gif' alt='imgsrc' onmouseover='style.cursor=\"hand\"' onclick='xoopsCodeImg(\"$textarea_id\", \"".htmlspecialchars(_ENTERIMGURL, ENT_QUOTES)."\", \"".htmlspecialchars(_ENTERIMGPOS, ENT_QUOTES)."\", \"".htmlspecialchars(_IMGPOSRORL, ENT_QUOTES)."\", \"".htmlspecialchars(_ERRORIMGPOS, ENT_QUOTES)."\");' />
+    &nbsp;<img src='".XOOPS_URL."/images/image.gif' alt='image' onmouseover='style.cursor=\"hand\"' onclick='openWithSelfMain(\"".XOOPS_URL."/imagemanager.php?target=".$textarea_id."\",\"imgmanager\",400,430);' />
+    &nbsp;<img src='".XOOPS_URL."/images/code.gif' alt='code' onmouseover='style.cursor=\"hand\"' onclick='xoopsCodeCode(\"$textarea_id\", \"".htmlspecialchars(_ENTERCODE, ENT_QUOTES)."\");' />
+    &nbsp;<img src='".XOOPS_URL."/images/quote.gif' alt='quote' onmouseover='style.cursor=\"hand\"' onclick='xoopsCodeQuote(\"$textarea_id\");'/><br />\n";
 
     $sizearray = array("xx-small", "x-small", "small", "medium", "large", "x-large", "xx-large");
     echo "<select id='".$textarea_id."Size' onchange='setVisible(\"xoopsHiddenText\");setElementSize(\"".$hiddentext."\",this.options[this.selectedIndex].value);'>\n";
     echo "<option value='SIZE'>"._SIZE."</option>\n";
-    foreach ( $sizearray as $size ) {
+    foreach ($sizearray as $size) {
         echo "<option value='$size'>$size</option>\n";
     }
     echo "</select>\n";
@@ -50,7 +55,7 @@ function xoopsCodeTarea($textarea_id, $cols=60, $rows=15, $suffix=null)
     $fontarray = array("Arial", "Courier", "Georgia", "Helvetica", "Impact", "Verdana");
     echo "<select id='".$textarea_id."Font' onchange='setVisible(\"xoopsHiddenText\");setElementFont(\"".$hiddentext."\",this.options[this.selectedIndex].value);'>\n";
     echo "<option value='FONT'>"._FONT."</option>\n";
-    foreach ( $fontarray as $font ) {
+    foreach ($fontarray as $font) {
         echo "<option value='$font'>$font</option>\n";
     }
     echo "</select>\n";
@@ -58,9 +63,9 @@ function xoopsCodeTarea($textarea_id, $cols=60, $rows=15, $suffix=null)
     $colorarray = array("00", "33", "66", "99", "CC", "FF");
     echo "<select id='".$textarea_id."Color' onchange='setVisible(\"xoopsHiddenText\");setElementColor(\"".$hiddentext."\",this.options[this.selectedIndex].value);'>\n";
     echo "<option value='COLOR'>"._COLOR."</option>\n";
-    foreach ( $colorarray as $color1 ) {
-        foreach ( $colorarray as $color2 ) {
-            foreach ( $colorarray as $color3 ) {
+    foreach ($colorarray as $color1) {
+        foreach ($colorarray as $color2) {
+            foreach ($colorarray as $color3) {
                 echo "<option value='".$color1.$color2.$color3."' style='background-color:#".$color1.$color2.$color3.";color:#".$color1.$color2.$color3.";'>#".$color1.$color2.$color3."</option>\n";
             }
         }
@@ -69,7 +74,7 @@ function xoopsCodeTarea($textarea_id, $cols=60, $rows=15, $suffix=null)
 
     echo "<br />\n";
     //Hack smilies move for bold, italic ...
-    $areacontent = isset( $GLOBALS[$textarea_id] ) ? $GLOBALS[$textarea_id] : '';
+    $areacontent = isset($GLOBALS[$textarea_id]) ? $GLOBALS[$textarea_id] : '';
     echo "<img src='".XOOPS_URL."/images/bold.gif' alt='bold' onmouseover='style.cursor=\"hand\"' onclick='setVisible(\"".$hiddentext."\");makeBold(\"".$hiddentext."\");' />&nbsp;<img src='".XOOPS_URL."/images/italic.gif' alt='italic' onmouseover='style.cursor=\"hand\"' onclick='setVisible(\"".$hiddentext."\");makeItalic(\"".$hiddentext."\");' />&nbsp;<img src='".XOOPS_URL."/images/underline.gif' alt='underline' onmouseover='style.cursor=\"hand\"' onclick='setVisible(\"".$hiddentext."\");makeUnderline(\"".$hiddentext."\");'/>&nbsp;<img src='".XOOPS_URL."/images/linethrough.gif' alt='linethrough' onmouseover='style.cursor=\"hand\"' onclick='setVisible(\"".$hiddentext."\");makeLineThrough(\"".$hiddentext."\");' />&nbsp;<input type='text' id='".$textarea_id."Addtext' size='20' />&nbsp;<input type='button' onclick='xoopsCodeText(\"$textarea_id\", \"".$hiddentext."\", \"".htmlspecialchars(_ENTERTEXTBOX, ENT_QUOTES)."\")' value='"._ADD."' /><br /><br /><textarea id='".$textarea_id."' name='".$textarea_id."' cols='$cols' rows='$rows'>".$areacontent."</textarea><br />\n";
     //Fin du hack
 }
@@ -80,13 +85,13 @@ function xoopsCodeTarea($textarea_id, $cols=60, $rows=15, $suffix=null)
 */
 function xoopsSmilies($textarea_id)
 {
-    $myts =& MyTextSanitizer::getInstance();
+    $myts =& MyTextSanitizer::sGetInstance();
     $smiles = $myts->getSmileys();
     if (empty($smiles)) {
         $db =& Database::getInstance();
         if ($result = $db->query('SELECT * FROM '.$db->prefix('smiles').' WHERE display=1')) {
             while ($smile = $db->fetchArray($result)) {
-            //hack smilies move for the smilies !!
+                //hack smilies move for the smilies !!
                 echo "<img src='".XOOPS_UPLOAD_URL."/".htmlspecialchars($smile['smile_url'])."' border='0' onmouseover='style.cursor=\"hand\"' alt='' onclick='xoopsCodeSmilie(\"".$textarea_id."\", \" ".$smile['code']." \");' />";
             //fin du hack
             }
@@ -95,7 +100,7 @@ function xoopsSmilies($textarea_id)
         $count = count($smiles);
         for ($i = 0; $i < $count; $i++) {
             if ($smiles[$i]['display'] == 1) {
-            //hack bis
+                //hack bis
                 echo "<img src='".XOOPS_UPLOAD_URL."/".htmlspecialchars($smiles[$i]['smile_url'])."' border='0' onmouseover='style.cursor=\"hand\"' alt='' onclick='xoopsCodeSmilie(\"".$textarea_id."\", \" ".$smiles[$i]['code']." \");' />";
             //fin du hack
             }
@@ -104,4 +109,4 @@ function xoopsSmilies($textarea_id)
     //hack for more
     echo "&nbsp;[<a href='#moresmiley' onmouseover='style.cursor=\"hand\"' alt='' onclick='openWithSelfMain(\"".XOOPS_URL."/misc.php?action=showpopups&amp;type=smilies&amp;target=".$textarea_id."\",\"smilies\",300,475);'>"._MORE."</a>]";
 }  //fin du hack
-
+;
