fbd2509d9e3081ba2572d408125252a639c32b74
Info Opensource <info.opensource@gmail.com>
2019-02-07 12:58:43 +0100

    // ! Fix PHP7 update modules/user

--- html/modules/user/class/AbstractViewAction.class.php
+++ html/modules/user/class/AbstractViewAction.class.php
@@ -4,64 +4,65 @@
  * @version $Id: AbstractViewAction.class.php,v 1.1 2007/05/15 02:34:49 minahito Exp $
  */
 
-if (!defined('XOOPS_ROOT_PATH')) exit();
+if (!defined('XOOPS_ROOT_PATH')) {
+    exit();
+}
 
 class User_AbstractViewAction extends User_Action
 {
-	var $mObject = null;
-	var $mObjectHandler = null;
+    public $mObject = null;
+    public $mObjectHandler = null;
+    // !Fix PHP7
+    public function __construct()
+    //public function User_AbstractViewAction()
+    {
+    }
 
-	function User_AbstractViewAction()
-	{
-	}
+    public function _getId()
+    {
+    }
 
-	function _getId()
-	{
-	}
+    public function &_getHandler()
+    {
+    }
 
-	function &_getHandler()
-	{
-	}
+    public function _setupObject()
+    {
+        $id = $this->_getId();
+        
+        $this->mObjectHandler =& $this->_getHandler();
+        
+        $this->mObject =& $this->mObjectHandler->get($id);
+    }
 
-	function _setupObject()
-	{
-		$id = $this->_getId();
-		
-		$this->mObjectHandler =& $this->_getHandler();
-		
-		$this->mObject =& $this->mObjectHandler->get($id);
-	}
+    /**
+     * _getPageAction
+     * 
+     * @param	void
+     * 
+     * @return	string
+    **/
+    protected function _getPageAction()
+    {
+        return _VIEW;
+    }
 
-	/**
-	 * _getPageAction
-	 * 
-	 * @param	void
-	 * 
-	 * @return	string
-	**/
-	protected function _getPageAction()
-	{
-		return _VIEW;
-	}
+    public function prepare(&$controller, &$xoopsUser, $moduleConfig)
+    {
+        $this->_setupObject();
+    }
 
-	function prepare(&$controller, &$xoopsUser, &$moduleConfig)
-	{
-		$this->_setupObject();
-	}
+    public function getDefaultView(&$controller, &$xoopsUser)
+    {
+        if ($this->mObject == null) {
+            return USER_FRAME_VIEW_ERROR;
+        }
+    
+        return USER_FRAME_VIEW_SUCCESS;
+    }
 
-	function getDefaultView(&$controller, &$xoopsUser)
-	{
-		if ($this->mObject == null) {
-			return USER_FRAME_VIEW_ERROR;
-		}
-	
-		return USER_FRAME_VIEW_SUCCESS;
-	}
-
-	function execute(&$controller, &$xoopsUser)
-	{
-		return $this->getDefaultView($controller, $xoopsUser);
-	}
+    public function execute(&$controller, &$xoopsUser)
+    {
+        return $this->getDefaultView($controller, $xoopsUser);
+    }
 }
-
-
