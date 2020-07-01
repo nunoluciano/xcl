88a82406718f8dbf33b99273ebc920ad8724dd6d
nunoluciano <nunoluciano@yahoo.com>
2020-07-01 22:41:10 +0200

    :hammer: [WIP] XCL Bootstrap Default + favicon.svg

---
+++ html/themes/xcl-bootstrap-default/manifesto.ini.php
@@ -0,0 +1,20 @@
+<?php
+/**
+
+[Manifesto]
+Name="XCL Bootstrap Default"
+Depends=Legacy_RenderSystem,legacy
+Url="https://github.com/xoopscube/xcl"
+Version="1.0.0"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=Nuno Luciano aka gigamaster
+ScreenShot="xcl-bootstrap-default.png"
+Description="XCL Bootstrap 4.5 theme with layout 2 columns, bottom 3 center-blocks,and modules templates."
+W3C=NG
+
+Licence="MIT"
+
+*/
