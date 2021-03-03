5df5bebad494d9be5e21e2fa6db742ad0d49e26f
Gigamaster <1905497+gigamaster@users.noreply.github.com>
2021-03-03 19:21:17 +0100

    :art: :construction: Theme Neumorphic templates + front_page

--- html/modules/legacy/kernel/Legacy_PublicControllerStrategy.class.php
+++ html/modules/legacy/kernel/Legacy_PublicControllerStrategy.class.php
@@ -37,7 +37,7 @@ class Legacy_PublicControllerStrategy extends Legacy_AbstractControllerStrategy
             $mid = $this->mController->mRoot->mContext->mXoopsModule->get('mid');
         } else {
             //
-            // If you does not have module_contoller, this request is to toppage or other pages of toppage.
+            // If you don't have module_contoller, this request is to toppage or other pages of toppage.
             //
 
             // $mid = preg_match("/index\.php$/i", xoops_getenv('PHP_SELF')) ? -1 : 0;
