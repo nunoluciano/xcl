7b86b74dad453d4b98c51f03d9ac300cfa94fee0
Gigamaster <1905497+gigamaster@users.noreply.github.com>
2021-03-27 23:22:23 +0100

    :penguin: :shirt: language :construction: fr utf-8 #238

---
+++ html/install/language/fr_utf8/finish.php
@@ -0,0 +1,14 @@
+<?php
+
+$content =
+	'<div class = "confirmInfo"> La dernière étape du programme permet une installation automatiquement des modules requis par XCL.<br>
+Votre nouvelle plateforme d\'application Web XCL sera opérationnelle! </div>
+<h3> Documentation XCL </h3>
+<h4> Manuel </h4>
+<p> XCL core fournit une fonctionnalité de recherche avancée, reportez-vous à la documentation respective intégrée dans chaque menu de module. </p>
+<h4> Assistance </h4>
+<p> Xupdate Manager dispose d\'une interface conviviale pour mettre à jour et installer des modules et thèmes gratuits créés pour les modules de la plateforme.
+Les extensions de fichier uniques Preload (préchargements) peuvent également être installées facilement via le backend. 
+Les extensions peuvent ajouter de nouvelles fonctionnalités ou modifier des fonctionnalités existantes. Le principe prévoit des améliorations sans altérer les fonctions système existantes. </p>
+<p> Visitez <a href="https://github.com/xoopscube/xcl/" rel="external">XOOPS Cube Project</a> pour plus de documentation, modules, extensions (preloads) et themes.</p>'
+;
